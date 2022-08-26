2c9628a055ff5c68bae758fce7f14fa2045a307e
cartertheesfeld <cartertheesfeld@hotmail.com>
2022-08-26 10:24:35 -0500

    Add project files.

---
+++ MyFirstProject/Source.cpp
@@ -0,0 +1,15 @@
+/*
+	Carter Theesfeld
+	C++ Fall 2022
+	Due: August 31, 2022
+	Lab 1 Introduction to C++ Programing
+	This lab is to show I can work with vs.code and github
+	*/
+
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!"<< " Purple";
+
+	return 0;
+}
